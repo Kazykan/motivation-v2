ba1413ecacf481602c3def9f807ae02a2c32bcc4
Kazykan <rufat@kocherbaev.ru>
2024-07-14 09:37:00 +0300

    add switch

---
+++ frontend/src/store/switch_edit.ts
@@ -0,0 +1,12 @@
+import { create } from "zustand"
+
+interface ISwitchEdit {
+  isEdit: boolean
+  setIsEdit: () => void
+}
+
+export const useSwitchEdit = create<ISwitchEdit>((set) => ({
+  isEdit: false,
+
+  setIsEdit: () => set((state) => ({ isEdit: !state.isEdit })),
+}))
