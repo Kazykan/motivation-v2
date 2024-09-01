3d41cceb90105f691f4df9784041ef4dd66f6c98
Kazykan <rufat@kocherbaev.ru>
2024-09-01 21:33:18 +0300

    fix config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [react()],
+// 	preview: {
+// 		port: 3000,
+// 		host: true,
+// 	},
+// });
+
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import fs from "fs"
+// import fs from "fs"
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +21,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  preview: {
+    port: 3000,
+    host: true,
   },
   // server: {
   //   host: "tg-mini-app.local",
