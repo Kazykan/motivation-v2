f8333dbed45664034dfd7a129b06684a60ffe4b2
Kazykan <rufat@kocherbaev.ru>
2024-09-08 23:35:41 +0300

    fix env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,26 +12,41 @@
 
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-// import fs from "fs"
+import { defineConfig, loadEnv } from "vite"
+import fs from "fs"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  preview: {
-    port: 3000,
-    host: true,
-  },
-  // server: {
-  //   host: "tg-mini-app.local",
-  //   port: 443,
-  //   https: {
-  //     key: fs.readFileSync("./.cert/localhost-key.pem"),
-  //     cert: fs.readFileSync("./.cert/localhost.pem"),
-  //   },
-  // },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  if (env.VITE_CONFIG_DEV === "True") {
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+      server: {
+        host: "tg-mini-app.local",
+        port: 443,
+        https: {
+          key: fs.readFileSync("./.cert/localhost-key.pem"),
+          cert: fs.readFileSync("./.cert/localhost.pem"),
+        },
+      },
+    }
+  } else {
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+      preview: {
+        port: 3000,
+        host: true,
+      },
+    }
+  }
 })
